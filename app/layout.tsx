776204e5b87456e13961d71ccce7a5a659001227
Abhishek Khanzode <abhi.geek2004@gmail.com>
2026-01-10 21:42:42 +0530

    intialized

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import LenisSmoothScroll from "./components/SmoothScroll/Lenis";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <LenisSmoothScroll />
         {children}
       </body>
     </html>
